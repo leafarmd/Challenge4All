5aa98df015c70d2ad5c85fa76e247133dbeec95a
Rafael Damasceno <rafael.damaceno@cwi.com.br>
2019-08-09 23:28:20 -0300

    initial commit and blank project

---
+++ Challenge4AllTests/Challenge4AllTests.swift
@@ -0,0 +1,34 @@
+//
+//  Challenge4AllTests.swift
+//  Challenge4AllTests
+//
+//  Created by Rafael Damasceno on 09/08/19.
+//  Copyright © 2019 Rafael Damasceno. All rights reserved.
+//
+
+import XCTest
+@testable import Challenge4All
+
+class Challenge4AllTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
